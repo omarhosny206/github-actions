1de9fb752247fc1bcfa875f682a49ff5e97b20d5
Omar Hosny Keshk <58389695+omarhosny206@users.noreply.github.com>
2023-09-14 00:35:25 +0300

    First commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello-world")
+public class HelloWorldController {
+    @GetMapping("/")
+    public String test() {
+        return "Hello World!";
+    }
+}
